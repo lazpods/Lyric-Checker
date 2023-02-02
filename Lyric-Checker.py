f07e61d005de344beb7002a4efc7202061504b61
lazpods <lazarjevtic@yahoo.com>
2023-02-02 14:02:55 -0500

    Added Lyric-Checker.py

--- Lyric-Checker.py
+++ Lyric-Checker.py
@@ -1,42 +1,16 @@
 from better_profanity import profanity
-import requests
+from lyricsgenius import Genius
 
 # Initialize the profanity filter
 profanity.load_censor_words()
 
-# Replace YOUR_ACCESS_TOKEN with your actual access token
-headers = {'Authorization': 'Bearer YHvbmVbaCWQ35JNuwADK2aazsbJs6iX--fMy_OTrZBSxGLCq6Hu7rG0E-bOcgoWA'}
+# Initialize the Genius API
+genius = Genius('FNwup5QIwPws1x76qmMZp57uESOY2YnNZOefk7zwS_jWTIJA5fNK2R2e_wkSK6iZ')
 
-# Replace SONG_NAME and ARTIST_NAME with the name of the song and artist you want to search for
-song_name = input("Enter desired song name: ")
-artist_name = input("Enter artist of desired song: ")
-query = f"{song_name} {artist_name}"
-response = requests.get(f'https://api.genius.com/search?q={query}', headers=headers)
+# Search for the song using inputs
+song_title = input("Enter song title: ")
+song_artist = input("Enter song artist: ")
 
-# Check if the request was successful
-if response.status_code == 200:
-    # Get the lyrics from the response
-    lyrics = response.json()['response']['song']['lyrics']
-
-    # Write the lyrics to a text file
-    with open('lyrics.txt', 'w') as file:
-        file.write(lyrics)
-else:
-    print("Request failed with status code:", response.status_code)
-
-
-def explicitCheck():
-    # Read the text file into a string
-    with open('lyrics.text', 'r') as file:
-        text = file.read()
-
-    # Check if the text contains any profanity
-    if profanity.contains_profanity(text):
-        print("Warning: This text contains profanity.")
-        text.close()
-    else:
-        print("The text is clean.")
-        text.close()
-
-
+# Get the lyrics for the song
+song = genius.search_song(song_title, artist=song_artist)
 
